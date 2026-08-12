85488114bc0cac8105ffe28f06acc82fbcb563ad
Ana Kamilly <anakamillyalves000@gmail.com>
2026-08-12 16:47:06 -0300

    wip: landing page com tailwind

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-
+Route:: view('/landing', 'landing');
 
 Route::get('/produtos', [ProdutoController::class, 'index']);
 Route::post('/produtos', [ProdutoController::class, 'store']);
